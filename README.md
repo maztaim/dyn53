aed7d9d5cb7f6c8b72d0549ce84a114740389ed9
Tim Bosse <maztaim@timbos.se>
2020-10-26 22:42:10 -0400

    Dynamic IP update for Route53.

---
+++ README.md
@@ -0,0 +1,47 @@
+<!--
+ Copyright 2020 Tim Bosse
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
+# dynip53.py
+Update Amazon Route53 hosted zones with public facing IP.
+
+## Usage
+You will need to set the following environment variables for AWS services.
+
+```
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+AWS_DEFAULT_REGION
+```
+
+You will need to create a dynip53.toml config that lives in the same directory as dyn53.py.
+
+## Example Configuration
+```toml
+url = "http://checkip.amazonaws.com/" # URL must return only your public IP address.
+
+# List of A records to update.
+records = [
+    "some.example.org",
+    "other.example.org",
+    "another.sample.net",
+]
+
+# List of hosted zones in Route53 where the records can be found.
+hostedzones = [
+    "example.org",
+    "sample.net",
+]
+```
---
+++ dynip53.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import requests
+import toml
+import boto3
+
+config_file = "dynip53.toml"
+config = toml.load(config_file)
+
+ip = requests.get(config["url"]).content.decode().strip()
+hosted_zones_list = config["hostedzones"]
+records_list = config["records"]
+
+r53 = boto3.client("route53")
+
+
+def get_hosted_zone(hosted_zone):
+    zone = r53.list_hosted_zones_by_name(
+        DNSName=hosted_zone,
+        MaxItems="1",
+    )
+    hosted_zone_name = ".".join(str(zone["HostedZones"][0]["Name"]).rsplit(".")[:-1])
+    hosted_zone_id = str(zone["HostedZones"][0]["Id"])
+    return {"name": hosted_zone_name, "id": hosted_zone_id}
+
+
+hosted_zones = [get_hosted_zone(hosted_zone) for hosted_zone in hosted_zones_list]
+
+
+def get_record(record, hosted_zone):
+    check_record_data = r53.list_resource_record_sets(
+        HostedZoneId=hosted_zone["id"],
+        StartRecordName=record,
+        StartRecordType="A",
+        MaxItems="1",
+    )
+
+    check_record = check_record_data["ResourceRecordSets"][0]["Name"]
+    record_value = check_record_data["ResourceRecordSets"][0]["ResourceRecords"][0][
+        "Value"
+    ]
+    full_record = record + "."
+    if full_record == check_record and record_value != ip:
+        return full_record
+
+
+records = [
+    get_record(record, hosted_zone)
+    for record in records_list
+    for hosted_zone in hosted_zones
+]
+
+
+def update_record(record, hosted_zone, ip):
+    update_record = r53.change_resource_record_sets(
+        HostedZoneId=hosted_zone["id"],
+        ChangeBatch={
+            "Comment": "Updated by dyn53.py.",
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": record,
+                        "Type": "A",
+                        "TTL": 60,
+                        "ResourceRecords": [
+                            {"Value": ip},
+                        ],
+                    },
+                },
+            ],
+        },
+    )
+    return update_record
+
+
+if not any(record is None for record in records):
+    updates = [
+        update_record(record, hosted_zone, ip)
+        for record in records
+        for hosted_zone in hosted_zones
+    ]
+
+    print(updates)
